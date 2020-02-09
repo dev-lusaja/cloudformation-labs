725b8c40e98b7ea043071d672808b664800dfc16
Oscar Sanchez <janoone52@gmail.com>
2020-02-08 19:42:44 -0500

    cloudfront example was added

---
+++ README.md
@@ -0,0 +1,28 @@
+Static web site using S3 + cloudfront
+-------------------------------------
+
+## requirements
+
+* aws cli (configured profile)
+
+## Deploy
+
+* First: enter to directory with desired stack
+
+* Second: execute the deploy
+
+~~~~
+make deploy
+~~~~
+
+## Delete
+
+~~~~
+make delete
+~~~~
+
+## Sync data with S3
+
+~~~~
+make sync-bucket
+~~~~
--- base.mk
+++ base.mk
@@ -1,8 +1,10 @@
-PROJECT_NAME = static-web-site
-BUCKET_NAME = aws.devlusaja.com # your domain
-DEPLOY_REGION = us-east-1 # Virginia
+.DEFAULT_GOAL := help
 
-deploy:
+DEPLOY_REGION = us-east-1 # Virginia
+DOMAIN = devlusaja.com
+PROJECT_NAME_BASE = WebSite
+
+deploy: ## Deploy stack on AWS: make deploy
 	@echo starting deployment
 	@aws cloudformation deploy \
 	--template-file ./cloudformation/master.yml \
@@ -14,12 +16,12 @@ deploy:
 	@echo deployment completed
 	@make sync-bucket
 
-events:
+events: ## Show stack events: make events
 	@aws cloudformation describe-stack-events  \
 	--stack-name $(PROJECT_NAME) \
 	--region ${DEPLOY_REGION} \
 
-delete: delete-stack delete-bucket
+delete: delete-stack delete-bucket ## Delete full stack on AWS: make delete
 	@echo deleted completed
 
 delete-stack:
@@ -32,6 +34,11 @@ delete-bucket:
 	@echo starting bucket delete
 	@aws s3 rb s3://$(BUCKET_NAME) --force
 
-sync-bucket:
+sync-bucket: ## Sync data with the bucket: make sync-bucket
 	@echo starting sync bucket
 	@aws s3 sync ./public s3://$(BUCKET_NAME) --acl public-read
+
+help:
+	@printf "\033[31m%-22s %-59s %s\033[0m\n" "Target" " Help" "Usage"; \
+	printf "\033[31m%-22s %-59s %s\033[0m\n"  "------" " ----" "-----"; \
+	grep -hE '^\S+:.*## .*$$' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' | sort | awk 'BEGIN {FS = ":"}; {printf "\033[32m%-22s\033[0m %-58s \033[34m%s\033[0m\n", $$1, $$2, $$3}'
