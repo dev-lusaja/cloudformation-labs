1160b32d619e04acd1615c7b7975309e4605431f
Oscar Sanchez <janoone52@gmail.com>
2022-01-30 12:11:43 -0500

    commit

--- base.mk
+++ base.mk
@@ -1,28 +1,33 @@
 .DEFAULT_GOAL := help
 
-DEPLOY_REGION = us-east-1 # Virginia
-DOMAIN = devlusaja.com
-PROJECT_NAME_BASE = WebSite
+DEPLOY_REGION  = us-east-1
+USERNAME_LOCAL = "$(shell whoami)"
+HASH           = $$(date +%s)
 
-deploy: ## Deploy stack on AWS: make deploy
+hash:
+	echo $(HASH)
+
+create_stack: ## Deploy stack on AWS: make deploy
 	@echo starting deployment
 	@aws cloudformation deploy \
-	--template-file ./cloudformation/master.yml \
-	--stack-name ${PROJECT_NAME} \
-	--parameter-overrides \
-		BucketName=$(BUCKET_NAME) \
+	--template-file $(CLOUDFORMATION_TEMPLATE) \
+	--stack-name ${STACK_NAME} \
+	--parameter-overrides $(PARAMETERS) \
 	--region ${DEPLOY_REGION} \
 	--capabilities CAPABILITY_NAMED_IAM
 	@echo deployment completed
-	@make sync-bucket
+	#@make sync-bucket
+
+remove_stack:
+	@echo starting stack delete
+	@aws cloudformation delete-stack \
+	--stack-name $(STACK_NAME) \
+	--region ${DEPLOY_REGION}
 
 events: ## Show stack events: make events
 	@aws cloudformation describe-stack-events  \
 	--stack-name $(PROJECT_NAME) \
-	--region ${DEPLOY_REGION} \
-
-delete: delete-stack delete-bucket ## Delete full stack on AWS: make delete
-	@echo deleted completed
+	--region ${DEPLOY_REGION}
 
 delete-stack:
 	@echo starting stack delete
